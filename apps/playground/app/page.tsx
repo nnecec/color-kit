532fd794a5227f1d02e8499f24384e694acf03d7
nnecec <nnecec@outlook.com>
2024-03-14 01:10:52 +0800

    --wip-- [skip ci]

--- apps/playground/app/page.tsx
+++ apps/playground/app/page.tsx
@@ -1,16 +1,17 @@
 'use client'
 
 import { useDeferredValue, useMemo, useState } from 'react'
-import { Controller, useForm, useWatch } from 'react-hook-form'
 
-import { ArrowLeftIcon, ArrowRightIcon, MoonIcon, PlusIcon, SunIcon, XMarkIcon } from '@heroicons/react/20/solid'
+import { CheckIcon, PlusIcon, XMarkIcon } from '@heroicons/react/20/solid'
+import { ArrowUpOnSquareIcon } from '@heroicons/react/24/outline'
 import clsx from 'clsx'
 import { colorsNamed, differenceEuclidean, formatHex, nearest, random } from 'culori'
 import { useClipboard } from 'foxact/use-clipboard'
-import { motion, useMotionValueEvent, useScroll } from 'framer-motion'
+import { motion, useMotionValueEvent, useScroll, useTransform } from 'framer-motion'
+import { atom, useAtom } from 'jotai'
 import { createPalette } from 'tailwind-plugin-palette'
 
-import { Button, Checkbox, Input, Switch, Tooltip } from '@nextui-org/react'
+import { Button, Checkbox, Input, Tooltip } from '@nextui-org/react'
 
 import { ColorPicker } from '../components/color-picker'
 
@@ -18,46 +19,58 @@ let names = Object.keys(colorsNamed)
 
 const randomColor = () => {
   const hex = formatHex(random())
-  const name = nearest<string>(names, differenceEuclidean())(hex, 1)[0]
+  const name = nearest<string>(names, differenceEuclidean())(hex, 1)[0]!
   return { hex, name }
 }
 const MotionButton = motion(Button)
 
+type Options = {
+  dark?: boolean
+  primary?: string
+  reversed?: boolean
+}
+
+const optionsAtom = atom<Options>({
+  dark: false,
+  primary: undefined,
+  reversed: false,
+})
+
+const colorsAtom = atom<{ hex: string; name: string }[]>([])
+const editingSwatchesAtom = atom(new Map())
+
 export default function RootPage() {
   const [inPaletteView, setInPaletteView] = useState(false)
   const [primaryHovered, setPrimaryHovered] = useState(false)
-  const [colors, setColors] = useState(new Map())
+  const [options, setOptions] = useAtom(optionsAtom)
+  const [colors, setColors] = useAtom(colorsAtom)
 
-  const { control, setValue } = useForm<{ dark?: boolean; primary?: string; reversed?: boolean }>()
+  const [editingSwatches, setEditingSwatches] = useAtom(editingSwatchesAtom)
 
-  const values = useWatch({ control })
-  const deferredValues = useDeferredValue({ colors, ...values })
+  const deferredValues = useDeferredValue({
+    colors: colors,
+    ...options,
+  })
   const palette = useMemo(
     () =>
       createPalette({
-        colors: Object.fromEntries(colors),
+        colors: Object.fromEntries(deferredValues.colors.map(color => [color.name, color.hex])),
         dark: deferredValues.dark,
         primary: deferredValues.primary,
         reversed: deferredValues.reversed,
       }),
-    [colors, deferredValues],
+    [deferredValues],
   )
 
-  console.log(palette)
+  const isEmptyPalette = colors.length === 0
 
-  const isEmptyPalette = !Object.keys(palette)?.length
-
-  const handleEditSubmit = () => {
+  const addSwatch = () => {
     let color = randomColor()
 
-    while (colors.has(color.name)) {
+    while (colors.some(({ name }) => name === color.name)) {
       color = randomColor()
     }
-
-    setColors(colors => {
-      colors.set(color.name, color.hex)
-      return new Map(colors)
-    })
+    setColors([...colors, color])
   }
 
   const { copied, copy } = useClipboard({ timeout: 2000 })
@@ -72,14 +85,19 @@ export default function RootPage() {
     }
   })
 
-  const removePrimaryColor = () => {
-    setValue('primary', undefined)
-  }
+  const titleY = useTransform(scrollY, [0, window.innerHeight], [0, window.innerHeight / 2.5])
+
+  console.log(palette)
 
   return (
-    <div className="h-screen w-screen">
+    <div className="relative h-screen w-screen">
       <div className="flex h-screen items-center justify-center">
-        <div className="text-center">
+        <motion.div
+          className="text-center"
+          style={{
+            y: titleY,
+          }}
+        >
           <Tooltip
             className="h-[80px] w-[500px]"
             content={
@@ -110,7 +128,7 @@ export default function RootPage() {
           >
             <h1 className="mb-4 text-6xl">tailwind-plugin-palette</h1>
           </Tooltip>
-        </div>
+        </motion.div>
       </div>
       <motion.div
         animate={inPaletteView ? 'show' : 'hide'}
@@ -130,12 +148,10 @@ export default function RootPage() {
             onPointerEnter={() => setPrimaryHovered(true)}
             onPointerLeave={() => setPrimaryHovered(false)}
           >
-            <Controller
-              control={control}
-              name="primary"
-              render={({ field }) => (
-                <ColorPicker className="z-10" onChange={value => field.onChange(value)} value={field.value} />
-              )}
+            <ColorPicker
+              className="z-10"
+              onChange={value => setOptions({ ...options, primary: value })}
+              value={options.primary}
             />
 
             <MotionButton
@@ -144,7 +160,7 @@ export default function RootPage() {
               isIconOnly
               onPress={() => {
                 setPrimaryHovered(false)
-                removePrimaryColor()
+                setOptions({ ...options, primary: undefined })
               }}
               radius="full"
               size="sm"
@@ -166,30 +182,30 @@ export default function RootPage() {
 
           <motion.div className="flex gap-3" layout="position">
             <Tooltip content="Add a swatch">
-              <Button isIconOnly onPress={handleEditSubmit} radius="full" size="sm">
+              <Button isIconOnly onPress={addSwatch} radius="full" size="sm">
                 <PlusIcon width={14} />
               </Button>
             </Tooltip>
 
-            <Controller
-              control={control}
-              name="dark"
-              render={({ field }) => (
-                <Checkbox isSelected={field.value} onValueChange={value => field.onChange(value)} size="lg">
-                  Dark
-                </Checkbox>
-              )}
-            />
+            <Checkbox
+              isSelected={options.dark}
+              onValueChange={value => setOptions({ ...options, dark: value })}
+              size="lg"
+            >
+              Dark
+            </Checkbox>
 
-            <Controller
-              control={control}
-              name="reversed"
-              render={({ field }) => (
-                <Checkbox isSelected={field.value} onValueChange={value => field.onChange(value)} size="lg">
-                  Reversed
-                </Checkbox>
-              )}
-            />
+            <Checkbox
+              isSelected={options.reversed}
+              onValueChange={value => setOptions({ ...options, reversed: value })}
+              size="lg"
+            >
+              Reversed
+            </Checkbox>
+
+            <Button disabled={isEmptyPalette} isIconOnly size="sm">
+              <ArrowUpOnSquareIcon width={14} />
+            </Button>
           </motion.div>
         </motion.div>
       </motion.div>
@@ -202,55 +218,92 @@ export default function RootPage() {
       >
         {isEmptyPalette ?
           <div>empty</div>
-        : Object.entries(palette).map(([name, shades]) => (
-            <div key={name}>
-              <div className="flex items-center gap-4">
-                <ColorPicker value={colors.get(name)} />
-                <Input classNames={{ base: 'w-auto' }} fullWidth={false} value={name} />
-              </div>
-              <motion.div
-                animate="show"
-                className="flex"
-                initial="hide"
-                transition={{
-                  staggerChildren: 0.01,
-                }}
-                variants={{
-                  hide: {
-                    opacity: 0,
-                  },
-                  show: {
-                    opacity: 1,
-                  },
-                }}
-              >
-                {Object.entries(shades).map(([step, shade], index) => (
-                  <motion.div
-                    className="relative w-full"
-                    key={index}
-                    variants={{
-                      hide: {
-                        opacity: 0,
-                      },
-                      show: {
-                        opacity: 1,
-                      },
+        : colors.map((swatch, index) => {
+            const editingSwatch = editingSwatches.get(index)
+
+            return (
+              <div key={swatch.hex}>
+                <div className="flex items-center gap-4">
+                  <ColorPicker
+                    className="z-10"
+                    defaultValue={swatch.hex}
+                    onChange={value => {
+                      const newSwatch = { hex: value, name: swatch.name }
+                      editingSwatches.set(index, newSwatch)
+                      setEditingSwatches(new Map(editingSwatches))
                     }}
-                  >
-                    <div className="mt-[100%]" />
-                    <Tooltip content={`${name}-${step}`}>
-                      <div
-                        className="absolute inset-[12%] rounded-full text-sm"
-                        style={{
-                          backgroundColor: shade,
+                  />
+
+                  <Input
+                    classNames={{ base: 'w-auto' }}
+                    defaultValue={swatch.name}
+                    fullWidth={false}
+                    onValueChange={value => {
+                      const newSwatch = { hex: swatch.hex, name: value }
+                      editingSwatches.set(index, newSwatch)
+                      setEditingSwatches(new Map(editingSwatches))
+                    }}
+                  />
+
+                  {editingSwatch && (editingSwatch.name !== swatch.name || editingSwatch.hex !== swatch.hex) ?
+                    <Button
+                      isIconOnly
+                      onPress={() => {
+                        const newSwatch = editingSwatches.get(index)
+                        editingSwatches.delete(index)
+                        setColors(colors.toSpliced(index, 1, newSwatch))
+                        setEditingSwatches(new Map(editingSwatches))
+                      }}
+                    >
+                      <CheckIcon width={14} />
+                    </Button>
+                  : null}
+                </div>
+                <motion.div
+                  animate="show"
+                  className="flex"
+                  initial="hide"
+                  transition={{
+                    staggerChildren: 0.01,
+                  }}
+                  variants={{
+                    hide: {
+                      opacity: 0,
+                    },
+                    show: {
+                      opacity: 1,
+                    },
+                  }}
+                >
+                  {!!palette[swatch.name] &&
+                    Object.entries(palette[swatch.name] as Record<string, string>).map(([step, shade], key) => (
+                      <motion.div
+                        className="relative w-full"
+                        key={key}
+                        variants={{
+                          hide: {
+                            opacity: 0,
+                          },
+                          show: {
+                            opacity: 1,
+                          },
                         }}
-                      />
-                    </Tooltip>
-                  </motion.div>
-                ))}
-              </motion.div>
-            </div>
-          ))
+                      >
+                        <div className="mt-[100%]" />
+                        <Tooltip content={`${swatch.name}-${step}`}>
+                          <div
+                            className="absolute inset-[12%] rounded-full text-sm"
+                            style={{
+                              backgroundColor: shade,
+                            }}
+                          />
+                        </Tooltip>
+                      </motion.div>
+                    ))}
+                </motion.div>
+              </div>
+            )
+          })
         }
       </div>
     </div>
--- apps/playground/components/color-picker.tsx
+++ apps/playground/components/color-picker.tsx
@@ -4,12 +4,17 @@ import { useRef } from 'react'
 
 import { Button } from '@nextui-org/react'
 
+import { usePropsValue } from '../utils/use-props-value'
+
 export const ColorPicker = ({
+  defaultValue,
   onChange,
   value,
   ...props
-}: { onChange?: (value: string) => void; value?: string } & ButtonProps) => {
+}: { defaultValue?: string; onChange?: (value: string) => void; value?: string } & Omit<ButtonProps, 'onChange'>) => {
   const ref = useRef<HTMLInputElement>(null)
+
+  const [val, setVal] = usePropsValue<string>({ defaultValue: defaultValue ?? '', onChange, value })
 
   return (
     <Button
@@ -18,9 +23,9 @@ export const ColorPicker = ({
       radius="full"
       size="sm"
       style={{
-        backgroundColor: value ?? 'rgba(255 255 255)',
+        backgroundColor: val ?? '#fff',
         backgroundImage:
-          value ? 'none' : (
+          val ? 'none' : (
             `url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill-opacity=".05"><rect x="8" width="8" height="8"/><rect y="8" width="8" height="8"/></svg>')`
           ),
         zIndex: 10,
@@ -29,10 +34,11 @@ export const ColorPicker = ({
     >
       <input
         className="invisible absolute left-0 size-0"
-        onChange={e => onChange?.(e.target.value)}
+        defaultValue={defaultValue}
+        onChange={e => setVal(e.target.value)}
         ref={ref}
         type="color"
-        value={value}
+        value={val}
       />
     </Button>
   )
